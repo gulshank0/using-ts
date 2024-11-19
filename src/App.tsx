2c512917e8e9d267c8a113641898bb1a74251e50
Gulshan Kumar <gulshanbahadur002@gmail.com>
2024-11-19 23:25:34 +0530

    Update App.tsx
    add

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,19 @@ function App() {
   return (
     <>
      
+interface Person {
+    name: string;
+    age: number;
+    isStudent: boolean;
+}
+
+const person: Person = {
+    name: "Bob",
+    age: 30,
+    isStudent: false
+};
+
+console.log(person);
 
 
 
